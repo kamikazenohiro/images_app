b50cd8fd77ec2b10663a455d6714a369b9ee50a2
kamikazenohiro <kamikazenohiro632@icloud.com>
2021-02-07 12:41:56 +0900

    アソシエーションとバリデーション

--- db/schema.rb
+++ db/schema.rb
@@ -35,8 +35,10 @@ ActiveRecord::Schema.define(version: 2021_02_07_022728) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content", null: false
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -53,4 +55,5 @@ ActiveRecord::Schema.define(version: 2021_02_07_022728) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "messages", "users"
 end
